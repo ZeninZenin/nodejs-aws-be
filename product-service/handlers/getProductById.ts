56cdb40a94d51f3f15434289b837bd0844ba5bc4
Зенин Сергей Андреевич (4016722) <szenin@dev.vtb>
2020-10-31 01:00:20 +0300

    feat: add parameter validation

--- product-service/handlers/getProductById.ts
+++ product-service/handlers/getProductById.ts
@@ -3,7 +3,16 @@ import 'source-map-support/register';
 import { PRODUCTS_MOCK } from '../mockData';
 
 export const getProductById: APIGatewayProxyHandler = async ({ pathParameters }) => {
-  const hit = PRODUCTS_MOCK.find(({ id }) => pathParameters.id === id);
+  const { id } = pathParameters;
+
+  if (isNaN(+id)) {
+    return {
+      statusCode: 400,
+      body: `ID must be a number`,
+    };
+  }
+
+  const hit = PRODUCTS_MOCK.find(product => product.id === id);
 
   if (!hit) {
     return {
